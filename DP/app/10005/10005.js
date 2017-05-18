77b5fd079e7aec8f97290381e575d74c6d7a76c3
sun1630 <szq1630@126.com>
2017-05-18 14:35:23 +0800

    1

--- DP/app/10005/10005.js
+++ DP/app/10005/10005.js
@@ -1,45 +1,4 @@
-﻿define(['vmProvide', 'share'], function (vm, dm) {
-
-    var model= new vm({
-        name: '',
-        data: {
-            System: dm.System,
-            Teller: dm.Teller,
-            //TellerName :{
-            //    value:shareData.Teller.Name,
-            //    metadata{}
-
-            //},
-            //Teller2: {
-            //    NAME: {
-            //        value: shareData.Teller.Name,
-            //        metadata: {
-            //        }
-            //    }
-            //},
-            TransNo: {
-                value: 'T00001',
-                metadata: {
-                    rule: {
-                        required: true,
-                        readony: true
-                    }
-                }
-            },
-            TransAmount: {
-                value: 1000,
-                metadata: {
-                    rule: {
-                        required: true
-                    },
-                    format: function (value) {
-                        return value;
-                    }
-                }
-            },
-            Address: []
-        }
-    })
-
-    return model;
+﻿define(['durandal/app', 'durandal/system', 'knockout', 'share','vmProvide'], function (app, system, ko, data,vmp) {
+   
+    return new vmp({});
 });
--- DP/vmProvide.js
+++ DP/vmProvide.js
@@ -1,86 +1,9 @@
 ﻿define(['knockout', 'knockout.validation'], function (ko) {
 
-    ko.validation.rules.pattern.message = 'Invalid.';
-
-    ko.validation.init({
-        registerExtenders: true,
-        messagesOnModified: true,
-        insertMessages: true,
-        parseInputAttributes: true,
-        messageTemplate: null
-    }, true);
-
-    ko.extenders.logChange = function (target, option) {
-        target.subscribe(function (newValue) {
-            console.log(option + ": " + newValue);
-            // utils.j2c(option, newValue);  后端操作接口 TODO
-        });
-        return target;
-    };
-    ko.subscribable.fn.cusFormat = function (format) {
-        var target = this;
-        var writeTarget = function (value) {
-            var stripped = value.replace(/[^0-9.-]/g, '');
-            target(parseFloat(stripped));
-        };
-        var result = ko.computed({
-            read: function () {
-                return target();
-            },
-            write: writeTarget
-        });
-        result.formatted = ko.computed({
-            read: function () {
-                return format(target());
-            },
-            write: writeTarget
-        });
-        result.isNegative = ko.computed(function () {
-            return target() < 0;
-        });
-        return result;
-    };
-
 
     return function (opt) {
-        var local = function (ctx) {
-            for (var d in opt.data) {
-                var field = opt.data[d];
-                this[d] = field instanceof Array ? ko.observableArray(field) : ko.observable(field).extend({ logChange: d });
-
-                if (field instanceof Object) {
-                    if (field.metadata && field.metadata.rule) {
-                        this[d].extend(field.metadata.rule); //验证加载
-                    }
-                    if (field.metadata && field.metadata.format) {
-                        this[d].cusFormat(field.metadata.format); //验证加载
-                    }
-                    field.value && this[d](field.value);
-                }
-            }
-            for (var a in opt.methods) {
-                this[a] = opt.methods[a].bind(Object.assign(this, ctx));
-            }
-
-            this.errors = ko.validation.group(this);
-            
-            return this;
-
-
-            //new Promise(function (resolve, reject) {
-            //    //require(opt.dependencies, function () {
-            //    //    this.dependencies = arguments;// {};
-            //    //    //           opt.dependencies.forEach((d, i) => {
-            //    //    //             this.dependencies[d] = arguments[i];
-            //    //    //           })
-            //    //    //opt.mounted.apply(this);
-            //    //    resolve(this);
-            //    //}.bind(this));
-            //}.bind(this));
-
+        return {
+            vmpName:'vmp-zhangsan'
         }
-
-        local.prototype = opt.context;
-        return local;
     };
 })
