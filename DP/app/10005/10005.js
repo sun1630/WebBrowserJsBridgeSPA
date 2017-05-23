8156bd2efbc76991cd1dbb9a0ff20de640435a0e
sun1630 <szq1630@126.com>
2017-05-23 08:59:48 +0800

    真数组成员

--- DP/app/10005/10005.js
+++ DP/app/10005/10005.js
@@ -40,11 +40,30 @@
                     }
                 }
             },
-            //Address: []
+            Province: {
+                value: [
+                    { code: '001', value: '北京市' },
+                    { code: '002', value: '上海市' }
+                ]
+            },
+            CityData: {
+                value: [
+                    { code: '001001', value: '海淀区', parent: '001' },
+                    { code: '001002', value: '昌平区', parent: '001' },
+                    { code: '002001', value: '浦东新区', parent: '002' },
+                    { code: '002002', value: '普陀区', parent: '002' },
+                ]
+            },
+            ProvinceCode: {
+                value: ''
+            },
+            City: {
+                value: []
+            }
         },
         methods: {
             changeBranchName: function () {
-                this.System.BranchNo('S0002',true);
+                this.System.BranchNo('S0002', true);
                 this.System.BranchName('上海分行');
             },
             alertTransNo: function () {
@@ -52,6 +71,21 @@
             },
             changeTransNo: function () {
                 this.TransNo(this.vmpTransNo(), false);
+            },
+            changepv: function () {
+                var code = this.ProvinceCode();
+                var tm = [];
+                this.CityData().forEach(function (item) {
+                    if (item.parent == code) {
+                        tm.push(item);
+                    }
+                })
+                //for (var c in this.CityData()) {
+                //    if (c.parent.code == this.Province().code) {
+                //        tm.push(c);
+                //    }
+                //}
+                this.City(tm);
             }
         }
     })
